4e8aba59cf9bd64db12870f9d942ba80d339ea4e
Babina Arora <babina.exam@gmail.com>
2019-08-02 10:02:23 +1000

    Updated test case for update feed tag service

--- src/app/services/update-feed-tag.service.spec.ts
+++ src/app/services/update-feed-tag.service.spec.ts
@@ -1,23 +1,22 @@
 
-import {inject, TestBed} from '@angular/core/testing';
+import { TestBed} from '@angular/core/testing';
 import { UpdateFeedTagService } from './update-feed-tag.service';
+import {of} from 'rxjs';
 
 const feedUrls = ['https:\/\/live.staticflickr.com\/65535\/48428813166_de6fb2de05_m.jpg',
   'https:\/\/live.staticflickr.com\/65535\/48428814716_59ec386b04_m.jpg'];
 
 describe('FetchFeedService', () => {
+  let updateFeedTagService: UpdateFeedTagService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [UpdateFeedTagService]
     });
+    updateFeedTagService = TestBed.get(UpdateFeedTagService);
   });
-  it(
-    'should get call publish feed to update the latest feed',
-    inject(
-      [ UpdateFeedTagService],
-      (updateFeedTagService: UpdateFeedTagService) => {
-        updateFeedTagService.publishFeed(feedUrls);
-      }
-    )
+  it('should call publish feed to update the latest feed', () => {
+      spyOn(updateFeedTagService, 'publishFeed').and.callThrough();
+      updateFeedTagService.publishFeed(feedUrls);
+    }
   );
 });
